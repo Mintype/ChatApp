cb6a6e1952c1e748c8909797d99077a6ebcd9d55
SmallPlayz <900raiyan@gmail.com>
2022-05-21 10:20:54 -0400

    hello world

---
+++ src/com/hackathon/Client.java
@@ -0,0 +1,14 @@
+package src.com.hackathon;
+
+import javax.swing.*;
+
+public class Client {
+    public static void main(String []args){
+        JFrame frame = new JFrame("Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        System.out.print("hi");
+    }
+}
