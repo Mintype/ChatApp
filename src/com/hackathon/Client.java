48c5fb1dbdc0d6ed9bc8b5f4dbf342805546061e
SmallPlayz <900raiyan@gmail.com>
2022-05-21 13:30:45 -0400

    hgiufeiq

--- src/com/hackathon/Client.java
+++ src/com/hackathon/Client.java
@@ -1,14 +1,24 @@
 package src.com.hackathon;
-import java.util.Scanner; 
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 import javax.swing.*;
 
-public class Client {
-    public static void main(String []args){
+public class Client implements Runnable {
+
+    private static Socket clientSocket = null;
+    public static PrintStream os = null;
+    private static DataInputStream is = null;
+    private static BufferedReader inputLine = null;
+    private static boolean closed = false;
+
+    public static void main(String []args) {
 
         int portNumber = 10334;
         String host = "localhost";
-        
+
         System.out.println("Now using host = " + host + ", portNumber = " + portNumber);
 
         System.out.println("Welcome To ChatApp");
@@ -18,12 +28,45 @@ public class Client {
         String message="";
         String full="";
         Boolean exit=false;
-        username=input.nextLine(); 
-        while(true){
-            message=input.nextLine();
-            full=username+": "+message;
-            System.out.println(message);
-            
+        System.out.print("Input your username: ");
+        username=input.nextLine();
+
+        try {
+            clientSocket = new Socket(host, portNumber);
+            inputLine = new BufferedReader(new InputStreamReader(System.in));
+            os = new PrintStream(clientSocket.getOutputStream());
+            is = new DataInputStream(clientSocket.getInputStream());
+            os.println(username);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (clientSocket != null && os != null && is != null) {
+            try {
+                new Thread(new Client()).start();
+                while (!closed) {
+                    os.println(inputLine.readLine().trim());
+                }
+                os.close();
+                is.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                System.err.println("IOException:  " + e);
+            }
+        }
+
+    }
+    public void run() {
+        String responseLine;
+        try {
+            while ((responseLine = is.readLine()) != null) {
+                System.out.println(responseLine);
+            }
+            closed = true;
+        } catch (IOException e) {
+            System.err.println("IOException:  " + e);
         }
     }
 }
